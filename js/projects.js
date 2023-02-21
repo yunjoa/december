c3cdbbe258600c8a6eff3dc098e46bff75dc356b
yunjoa <52683681+yunjoa@users.noreply.github.com>
2023-02-21 19:42:15 +0900

    2

--- js/projects.js
+++ js/projects.js
@@ -21,3 +21,39 @@ const closeModal = () => {
 };
 overlay.addEventListener("click", closeModal);
 openBtn.addEventListener("click", openModal);
+
+let result;
+
+$.getJSON('data/data.json', function(data) {
+  for(key in data){
+        
+    console.log(data.projects)
+    result += `
+      <div class="swiper-slide">
+  
+      <div
+      class="slide-image"
+      style="
+        background-image: url(${data.projects[key].imgSrc});
+      "
+    ></div>
+      <div class="slide-image-con">
+        <h4>${data.projects[key].title}</h4>
+        <p>${data.projects[key].details}</p>
+        <div class="dates">dates or cathegory</div>
+      </div>
+      </div>
+      `
+      result += list;
+      console.log(result)
+    }
+
+
+
+
+    $('#swiperWrapper').html(result);
+      
+
+      })
+
+
